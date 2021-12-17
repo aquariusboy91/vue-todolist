44451f56760a21bb3d99ef09abb48ac7f491ca9d
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-12-17 12:56:27 +0100

    add class check

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Questa volta però ogni todo sarà 
+// un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false)
+//  che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all’interno di una lista, 
+// un item per ogni todo.
+// Se la proprietà done è uguale a true, 
+// visualizzare il testo del todo sbarrato.
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    todos: [
+
+        {
+            text: 'lorem ipsum gggg',
+            done: true
+        },
+        {
+            text: 'lorem ipsum papapa',
+            done: true
+        },
+        {
+            text: 'lorem ipsum dododo',
+            done: true
+        },
+        {
+            text: 'lorem ipsum eeeee',
+            done: false
+        },
+        {
+            text: 'lorem ipsum aaaa',
+            done: false
+        }
+    ]
+  }
+ 
+    
+
+})
