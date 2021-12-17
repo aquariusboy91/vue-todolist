074a3a769f0ce9ba7706dda353c666757c7c9ba2
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-12-17 15:07:59 +0100

    add input/add new todo

--- js/script.js
+++ js/script.js
@@ -13,10 +13,17 @@
 // Visualizzare a fianco ad ogni item ha una “x”: 
 // cliccando su di essa, il todo viene rimosso dalla lista.
 
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante 
+// “aggiungi”: cliccando sul pulsante, il testo digitato
+//  viene letto e utilizzato per creare un nuovo todo, che 
+//  quindi viene aggiunto alla lista dei todo esistenti.
+
 
 var app = new Vue({
   el: '#app',
   data: {
+      todo: '',
     todos: [
 
         {
@@ -45,6 +52,11 @@ var app = new Vue({
     remove: function (index) {
         this.todos.splice(index,1)
 
+    },
+    addObj: function () {
+        this.todos.push({ text : this.todo});
+       console.log(this.todo)
+        
     }
   }
  
